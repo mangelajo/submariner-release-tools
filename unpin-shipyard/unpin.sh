562319ca5453ca5294c2a293417f452d2dabfe4d
Miguel Angel Ajo <majopela@redhat.com>
2020-06-25 10:48:53 +0200

    Add shipyard-unpin script
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

---
+++ unpin-shipyard/unpin.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e -x
+GH_USER=${GH_USER:-mangelajo}
+
+projects="admiral lighthouse submariner submariner-operator"
+
+for project in $projects; do
+
+	rm -rf $project || true
+	git clone git@github.com:submariner-io/${project}.git
+	pushd $project
+	git remote add $GH_USER git@github.com:$GH_USER/${project}.git
+	git checkout -B unpin-shipyard
+	sed -i 's/shipyard-dapper-base:.*$/shipyard-dapper-base/' Dockerfile.dapper
+	git commit -a -s -S -m "Unpin shipyard after release"
+	git push -f $GH_USER unpin-shipyard
+	hub pull-request -m "Unpin shipyard after release" || true
+	popd
+
+done
